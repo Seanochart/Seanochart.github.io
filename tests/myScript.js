0262c10a3dd97c3b39ec07552d00e1dd0b107787
Seanochart <55293773+Seanochart@users.noreply.github.com>
2020-01-15 14:10:52 -0500

    Update myScript.js

--- tests/myScript.js
+++ tests/myScript.js
@@ -1,1 +1,19 @@
+document.write("Using Math.sqrt <br/>")
+for(var k = 0; k < 9; k++)
+  {
+    var ans = Math.sqrt(k);
+    document.write(ans + "<br/>");
+  }
+
+
+document.write("<br/>  Using Math.random")
+
+var beginning = 40;
+
+while(beginning > 0)
+   {
+    document.write("<br />Current Count: " + beginning )
+    beginning = Math.random () * 11;
+     beginning--;
+   }
 
